4cc34fb0138ec24fe9627725dcd08fab2f0af5a0
verasveras <lisaveras@gmail.com>
2018-03-21 17:15:23 -0400

    fix: remove cache folder

--- .cache/layouts/index.js
+++
@@ -1,7 +0,0 @@
-
-  import React from "react"
-  import Component from "/Users/veras/Desktop/pct/src/layouts/index.js"
-  import data from "/Users/veras/Desktop/pct/.cache/json/layout-index.json"
-
-  export default (props) => <Component {...props} {...data} />
-  
